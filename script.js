82a0b9c1bc185b69d016871615f53bda9889cbe3
sioodmy <sioodmy@tuta.io>
2022-09-30 14:21:44 +0200

    init: first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll(".value");
+
+const speed = 1500;
+counters.forEach((counter) => {
+  const animate = () => {
+    const value = +counter.getAttribute("akhi");
+    // const speed = +counter.getAttribute("speed");
+    const data = +counter.innerText;
+
+    const time = value / speed;
+    if (data < value) {
+      counter.innerText = Math.ceil(data + time);
+      setTimeout(animate, 1);
+    } else {
+      counter.innerText = value;
+    }
+  };
+
+  animate();
+});
